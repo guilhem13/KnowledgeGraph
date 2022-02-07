16bc6f2dd477cf190c2ea743d89c60911754584c
guilhem13 <guilhem.maille@gmail.com>
2022-02-07 22:32:47 +0100

    multiprocessing batch added

--- controller/mainprocess.py
+++ controller/mainprocess.py
@@ -7,7 +7,9 @@ import json
 import redis 
 from multiprocessing import Process
 import multiprocessing as mp
-
+import urllib.request
+import threading
+from multiprocessing import cpu_count
 
 redis_host = "localhost"
 redis_port = 6379
@@ -83,24 +85,33 @@ class Pipeline():
         f.close()
         return True
     """
+    
 ###########################################################################################################
-    def multi_process(self, data, out_queue):         
-        processor = Textprocessed(data.link[0])            
-        text_processed = processor.get_data_from_pdf()
-        data.entities_include_in_text = processor.find_entities_in_raw_text()
-        data.entities_from_reference = self.get_references(text_processed)
-        out_queue.put(data)
+    def multi_process(self, data, out_queue):
+        if urllib.request.urlopen("http://172.17.0.3:5000/"):       
+            processor = Textprocessed(data.link[0])            
+            text_processed = processor.get_data_from_pdf()
+            data.entities_include_in_text = processor.find_entities_in_raw_text()
+            data.entities_from_reference = self.get_references(text_processed)
+            out_queue.put(data)
 
+    
     def make_traitement_pipeline(self,out_queue): 
         arxiv_data = Data.get_set_data(self.start)
-        workers = [ mp.Process(target=self.multi_process, args=(ele, out_queue) ) for ele in arxiv_data ]
-
-        for work in workers: work.start()
-        for work in workers: work.join(timeout=3)
-
         res_lst = []
-        for j in range(len(workers)):
-            res_lst.append(out_queue.get())
+        for i in range(0,len(arxiv_data),10):
+            print(i)
+            temp =arxiv_data[i:i+10]
+            workers = [ mp.Process(target=self.multi_process, args=(ele, out_queue) ) for ele in temp]
+            #s = threading.Semaphore(4)
+            for work in workers:
+                #with s:
+                work.start()
+            for work in workers: work.join(timeout=3)
+
+            #res_lst = []
+            for j in range(len(workers)):
+                res_lst.append(out_queue.get())
 
         f = open("test.json", "a")
         for test in res_lst: 
@@ -108,7 +119,9 @@ class Pipeline():
         f.close()
      # TODO récolter le nombre de coeur pour ensuite le mettre sur le code
      # gérer le problème quand c'est 10000  
+     #https://blog.ruanbekker.com/blog/2019/02/19/parallel-processing-with-python-and-multiprocessing-using-queue/
+     #https://towardsdatascience.com/pool-limited-queue-processing-in-python-2d02555b57dc
 
-    out_queue = mp.Queue()
+    
 
     
