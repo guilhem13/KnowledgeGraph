631287fab67cea4fd454f672b1fae7da73cf0b32
guilhem13 <guilhem.maille@gmail.com>
2022-02-26 14:46:14 +0100

    grobid script

--- nlpmodel/grobidmodel/grobidscript.py
+++ nlpmodel/grobidmodel/grobidscript.py
@@ -1,4 +1,6 @@
 from grobid.client import GrobidClient
+import sys 
+print(sys.path)
 
 client = GrobidClient("localhost","8070")
-rsp = client.serve("processReferences","test2.pdf", consolidate_header=1)
+rsp = client.serve("processReferences","/home/guigui/Documents/KnowledgeGraph/nlpmodel/grobidmodel/test2.pdf", consolidate_header=1)
